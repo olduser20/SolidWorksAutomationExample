5fef801b84b5b6c7d76bab1df1b837262094f042
Saber Nemati <m.saber.n@gmail.com>
2019-06-27 11:17:14 +0430

    Add project files.

---
+++ BlankAddin/TaskpaneHostUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace BlankAddin
+{
+    [ProgId(TaskpaneIntegration.SWTASKPANE_PROGID)]
+    public partial class TaskpaneHostUI : UserControl
+    {
+        public TaskpaneHostUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BlankAddin/TaskpaneIntegration.cs
@@ -0,0 +1,47 @@
+﻿
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swpublished;
+
+
+namespace BlankAddin
+{
+    /// <summary>
+    /// Our solidworks taskpane add-in
+    /// </summary>
+    public class TaskpaneIntegration : ISwAddin
+    {
+        #region Private Members
+
+        private int mSwCookie;
+        private TaskpaneViewHandler mTaskpaneView;
+
+
+        private TaskpaneHostUI mTaskpaneHost;
+        private SldWorks mSolidworksApplication;
+
+
+        
+
+        #endregion
+
+
+        #region Public Members
+
+        public const string SWTASKPANE_PROGID = "BlankAddin.TaskpaneHost";
+
+        #endregion
+
+
+        public bool ConnectToSW(object ThisSW, int Cookie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DisconnectfromSW()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
